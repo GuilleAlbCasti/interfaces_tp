993ee29e898e660611f7e5cb6e94d29979305d10
GuilleAlbCasti <guille_casti@hotmail.com>
2025-09-29 16:51:32 -0300

    finalizando la base del login, falta animaciones

--- TP2/js/carga_footer.js
+++ TP2/js/carga_footer.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('html/footer.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('footer').innerHTML = data;
+            document.getElementById('funcion_footer').innerHTML = data;
         });
 });
