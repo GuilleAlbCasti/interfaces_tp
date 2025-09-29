d064ce1e672e167579813413a8916e43f0b9ec87
GuilleAlbCasti <guille_casti@hotmail.com>
2025-09-29 15:09:23 -0300

    preparando para agregar footer

---
+++ TP2/js/carga_footer.js
@@ -0,0 +1,9 @@
+
+// FUNCIÓN PARA MOSTRAR EL FOOTER
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('html/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
