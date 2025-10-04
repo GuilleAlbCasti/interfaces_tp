09fda13ffbca2a0dd62a8f7214c71a22f0e674e7
GuilleAlbCasti <guille_casti@hotmail.com>
2025-10-04 13:51:37 -0300

    Armando el html de registro

--- TP2/js/carga_footer.js
+++ TP2/js/carga_footer.js
@@ -1,7 +1,7 @@
 
 // FUNCIÓN PARA MOSTRAR EL FOOTER
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('html/footer.html')
+    fetch('/TP2/html/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('funcion_footer').innerHTML = data;
